a075711b75db05d045b601fe1b8d6c5d60b7a3e6
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-03-14 11:37:05 -0300

    Remove var_dump

--- fabricantes/visualizar.php
+++ fabricantes/visualizar.php
@@ -5,9 +5,6 @@ require_once "../src/funcoes-fabricantes.php";
 /* Chamando a função responsável por carregar os dados dos Fabricantes */
 $listaDeFabricantes = listarFabricantes($conexao);
 ?>
-
-<pre><?=var_dump($listaDeFabricantes)?></pre>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
