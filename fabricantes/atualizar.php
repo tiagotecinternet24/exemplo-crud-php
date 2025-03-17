a8f3680e6c502ea0bb154cbc3a8ad10a16f46965
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-03-17 11:25:38 -0300

    Update atualizar.php

--- fabricantes/atualizar.php
+++ fabricantes/atualizar.php
@@ -7,9 +7,6 @@ $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 /* Chamando a função para carregar os dados de um fabricante */
 $fabricante = listarUmFabricante($conexao, $id);
 ?>
-
-<pre><?=var_dump($fabricante)?></pre>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -26,9 +23,14 @@ $fabricante = listarUmFabricante($conexao, $id);
         <hr>
 
         <form action="" method="post" class="w-25">
+            <!-- Campo oculto (hidden): o formulário/servidor "sabe"
+            do valor, mas não mostra para o usuário -->
+            <input type="hidden" name="id" value="<?=$fabricante['id']?>">
+
             <div class="mb-3">
                 <label for="nome" class="form-label">Nome:</label>
-                <input class="form-control" required type="text" name="nome" id="nome">
+                <input value="<?=$fabricante['nome']?>" 
+                class="form-control" required type="text" name="nome" id="nome">
             </div>
             <button class="btn btn-warning" type="submit" name="atualizar">
                 Atualizar fabricante</button>
