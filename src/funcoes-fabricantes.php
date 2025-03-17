3be8428e13baf5f238560522c322c5e4f532b440
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-03-17 09:16:39 -0300

    Aplica try/catch ao listarFabricantes

--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -7,14 +7,18 @@ require_once "conecta.php";
 function listarFabricantes(PDO $conexao):array {
     $sql = "SELECT * FROM fabricantes ORDER BY nome";
 
-    /* Preparando o comando SQL ANTES de executar no servidor
-    e guardando em memória (variável consulta ou query) */
-    $consulta = $conexao->prepare($sql);
-    
-    /* Executando o comando no banco de dados */
-    $consulta->execute();
-    
-    /* Busca/Retorna todos os dados provenientes da execução da consulta
-    e os transforma em um array associativo */
-    return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    try {
+        /* Preparando o comando SQL ANTES de executar no servidor
+        e guardando em memória (variável consulta ou query) */
+        $consulta = $conexao->prepare($sql);
+        
+        /* Executando o comando no banco de dados */
+        $consulta->execute();
+        
+        /* Busca/Retorna todos os dados provenientes da execução da consulta
+        e os transforma em um array associativo */
+        return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    } catch (Exception $erro) {
+        die("Erro: ".$erro->getMessage());
+    }   
 }
