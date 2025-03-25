638c083bf29101b08c267e1680953169e2c789b7
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-03-25 10:54:13 -0300

    Adiciona front básico

--- produtos/visualizar.php
+++ produtos/visualizar.php
@@ -21,6 +21,17 @@ $listaDeProdutos = listarProdutos($conexao);
 
         <p><a class="btn btn-primary btn-sm" href="inserir.php">Inserir novo produto</a></p>
 
+        <div class="row">
+            <div class="col-sm-6">
+                <article class="bg-body-secondary p-2">
+                    <h3>Nome do produto...</h3>
+                    <h4>Fabricante do produto...</h4>
+                    <p><b>Preço: </b> do produto</p>
+                    <p><b>Quantidade: </b> do produto</p>
+                </article>
+            </div>
+        </div>
+
 
     </div>
 
