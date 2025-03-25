b830ddf0d9290683cd1d9b728a4aec5c851a78f9
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-03-25 09:48:11 -0300

    Link dinâmico com 2 parâmetros

--- fabricantes/excluir.php
+++ fabricantes/excluir.php
@@ -1,3 +1,12 @@
+<?php
+require_once "../src/funcoes-fabricantes.php";
+$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+
+/* Verificando se houve o "SIM" para excluir */
+if(isset($_GET['confirmar-exclusao'])){
+    
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -15,7 +24,8 @@
         <p> Deseja realmente excluir este fabricante?</p>
         
         <a href="visualizar.php" class="btn btn-secondary">Não</a>
-        <a href="" class="btn btn-danger">Sim</a>        
+        <a href="?id=<?=$id?>&confirmar-exclusao" class="btn btn-danger">
+        Sim</a>        
     </div>
 
 </div>
