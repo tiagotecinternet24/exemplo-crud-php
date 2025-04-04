a650b94a32e29ff8c4514cb247d732fa10722854
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-04-04 09:01:35 -0300

    Carrega fabricantes

--- produtos/atualizar.php
+++ produtos/atualizar.php
@@ -1,5 +1,9 @@
 <?php
 require_once "../src/funcoes-produtos.php";
+
+require_once "../src/funcoes-fabricantes.php";
+$listaDeFabricantes = listarFabricantes($conexao);
+
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 $produto = listarUmProduto($conexao, $id);
 ?>
@@ -38,9 +42,13 @@ $produto = listarUmProduto($conexao, $id);
                 <label class="form-label" for="fabricante">Fabricante:</label>
                 <select class="form-select" name="fabricante" id="fabricante" required>
                     <option value=""></option>
-                    <option value="">Fabricante 1...</option>
-                    <option value="">Fabricante 2...</option>
-                    <option value="">Fabricante 3...</option>
+
+                <?php foreach($listaDeFabricantes as $fabricante): ?>
+                    <option value="<?=$fabricante['id']?>">
+                        <?=$fabricante['nome']?>
+                    </option>
+                <?php endforeach; ?>
+                
                 </select>
             </div>
             <div class="mb-3">
