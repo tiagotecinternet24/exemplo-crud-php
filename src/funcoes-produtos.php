ea3a82d3a46a03a03579a4a192e0a9e097caa998
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-04-04 08:35:23 -0300

    Faz listarUmProduto

--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -44,5 +44,15 @@ function inserirProduto(
 }
 
 function listarUmProduto(PDO $conexao, int $idProduto):array {
-    
+    $sql = "SELECT * FROM produtos WHERE id = :id";
+ 
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta->bindValue(":id", $idProduto, PDO::PARAM_INT);
+        $consulta->execute();
+        return $consulta->fetch(PDO::FETCH_ASSOC);
+    }
+        catch (Exception $erro) {
+            die("Erro ao inserir produto: ".$erro->getMessage());
+    }   
 }
